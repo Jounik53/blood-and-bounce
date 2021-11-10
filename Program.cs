416308dd1ee2838c42f7745723f7082b77458c19
Jounik <Joun_aleks@mail.ru>
2021-11-10 19:36:14 +0300

    First commit: initialization project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blood_and_bounce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
